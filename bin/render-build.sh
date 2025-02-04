a24627404c7b645411e63c4a24f4ce0f9105b73f
alvaroagsn <alvarogsilva.neto@gmail.com>
2025-02-04 10:35:40 -0300

    .

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,13 +8,13 @@ bundle install
 # Instala as dependências do Node (caso tenha frontend)
 cd frontend
 npm install
-npm run build
 npm install asciify-image --save
+npm run build
 cd ..
 
 # Compila os assets do Rails
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# Realiza as migrações do banco de dados (se necessário)
+RAILS_ENV=${RAILS_ENV:-development}
 bundle exec rails db:migrate
